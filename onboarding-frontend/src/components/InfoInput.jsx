d38e2929d172d29d0bf7d721d82100a9b2d14ccf
TonyJiashiChen <tonychen9661@gmail.com>
2023-08-03 22:44:14 +1000

    carbon footprint calculator

--- onboarding-frontend/src/components/InfoInput.jsx
+++ onboarding-frontend/src/components/InfoInput.jsx
@@ -1,20 +1,23 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button } from "@mui/material";
+import React from "react";
+import StepTwo from "./StepTwo";
 
-export function InfoInput({
-  activeStep,
-  steps,
-  nextStep,
-  lastStep
-}) {
+export function InfoInput({ activeStep, steps, nextStep, lastStep }) {
   return (
     <>
-      <Button variant="contained" onClick={lastStep}>Back</Button>
-      {
-        activeStep === steps.length - 1 ?
-          <Button variant="contained" onClick={nextStep}>Finish</Button> :
-          <Button variant="contained" onClick={nextStep}>Next</Button>
-      }
+      <StepTwo />
+      <Button variant="contained" onClick={lastStep}>
+        Back
+      </Button>
+      {activeStep === steps.length - 1 ? (
+        <Button variant="contained" onClick={nextStep}>
+          Finish
+        </Button>
+      ) : (
+        <Button variant="contained" onClick={nextStep}>
+          Next
+        </Button>
+      )}
     </>
-  )
+  );
 }
