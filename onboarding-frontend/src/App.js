358d915dc7d30246fa20bea8b9660077f38dadbd
jirathip-k <jkun0008@student.monash.edu>
2023-08-05 08:15:43 +1000

    Add basic landing page using react-spring

--- onboarding-frontend/src/App.js
+++ onboarding-frontend/src/App.js
@@ -2,16 +2,27 @@ import React, { useState } from "react";
 import "./App.css";
 import { Header } from "./layout/Header";
 import { Main } from "./layout/Main";
+import LandingPage from "./layout/LandingPage"
 import { Hidden, useTheme } from "@mui/material";
 
 function App() {
   const theme = useTheme();
+
+  const [landingToggled, setLandingToggled] = useState(true);
+
+  const handleLandingToggle = () => {
+    setLandingToggled(prev => !prev)
+  }
 
   return (
     <div style={{
       backgroundColor: theme.palette.background.default,
       height: '100vh'
     }}>
+      <LandingPage
+        landingToggled={landingToggled}
+        handleLandingToggle={handleLandingToggle}
+      />
       <Header />
       <Main />
     </div>
---
+++ onboarding-frontend/src/layout/LandingPage.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useTransition, a } from "react-spring"
+import { useTheme } from "@mui/material";
+import Typography from '@mui/material/Typography';
+import Button from "@mui/material/Button";
+
+import Trail from '../components/Trail'
+
+
+const LandingPage = (props) => {
+    const theme = useTheme()
+
+    const fullscreenPage = useTransition(props.landingToggled,{
+        from: {opacity: 1, transform: "scale(1)"},
+        enter: {opacity: 1, transform: "scale(1)"},
+        leave: {opacity: 0, transform: "translate3d(-50%,0,0)"},
+        config: {duration: 750}
+    })
+
+
+    return (
+        <div>
+        {fullscreenPage((styles, item) => item && <a.div style={{
+            ...styles,
+            position: "fixed",
+            maxHeight: "100vh",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: 'column',
+            justifyContent: "center",
+            backgroundColor: theme.palette.primary.main,
+            zIndex: 10
+        }}
+        
+        >
+            <div
+                style={{
+                    display: 'flex',
+                    maxHeight: '30vh',
+                    flexDirection: 'row',
+                    marginBottom: '5rem'
+                }}
+            >
+            <img src='/favicon-white.png' style={{height: '32px', marginRight: '1rem', marginTop: '0.5rem'}} alt='logo' />
+            <Typography variant="h3" color='common.white'>
+                Carbon Visualizer
+            </Typography>
+            </div>
+
+            <Trail open={props.landingToggled}>
+                <Typography variant="h4" color='common.white'>Let's calculate</Typography>
+                <Typography variant="h4" color='common.white'>your carbon</Typography>
+                <Typography variant="h4" color='common.white'>footprint</Typography>
+            </Trail>
+            
+
+            <Button 
+                variant="contained" 
+                size="large"
+                onClick={props.handleLandingToggle}
+            >
+                Start
+            </Button>
+
+        </a.div>)}
+        </div>
+    )
+}
+
+export default LandingPage
