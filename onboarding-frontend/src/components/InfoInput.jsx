56b66c4dd34244ba2e3c734474aa05d362a601d5
songjincheng <json0046@student.monash.edu>
2023-08-06 05:46:43 +1000

    change the layout, adjust to mobile client, add Accordion

--- onboarding-frontend/src/components/InfoInput.jsx
+++ onboarding-frontend/src/components/InfoInput.jsx
@@ -1,131 +1,272 @@
-import { Button } from "@mui/material";
+import {Button, Box, Divider, AccordionSummary, Accordion, AccordionDetails} from "@mui/material";
 import React, {useState} from "react";
-import { TextField, Typography, Grid, InputAdornment } from "@mui/material";
-import { useEffect } from "react";
-import { useTheme } from '@emotion/react';
+import {TextField, Typography, Grid, InputAdornment} from "@mui/material";
+import {useEffect} from "react";
+import {useTheme} from '@emotion/react';
 import useMediaQuery from "@mui/material/useMediaQuery";
+import {blue} from "@mui/material/colors";
 
-// const activities = [
-//   {
-//     name: 'Electricity',
-//     unit: 'kWh',
-//     icon: '⚡',
-//   },
-//   {
-//     name: 'Gas',
-//     unit: 'MJ',
-//     icon: '🔥'
-//   },
-//   {
-//     name: 'Private Transport',
-//     unit: 'km',
-//     icon: '🚗'
-//   }
-// ]
+import './InfoInput.css'
 
-export function InfoInput({ activeStep, steps, nextStep, lastStep, electricity, gas, car, result, setElectricity, setGas, setCar, setResult }) {
+export function InfoInput({
+                              activeStep,
+                              steps,
+                              nextStep,
+                              lastStep,
+                              electricity,
+                              gas,
+                              car,
+                              result,
+                              setElectricity,
+                              setGas,
+                              setCar,
+                              setResult
+                          }) {
 
-  const [electricityUsage, setElectricityUsage] = useState(0);
-  const [gasUsage, setGasUsage] = useState(0);
-  const [carUsage, setCarUsage] = useState(0);
+    const [electricityUsage, setElectricityUsage] = useState(0);
+    const [gasUsage, setGasUsage] = useState(0);
+    const [carUsage, setCarUsage] = useState(0);
 
-  const theme = useTheme();
-  const isScreenLargerThanMd = useMediaQuery(theme.breakpoints.up('md'));
+    const theme = useTheme();
+    const isScreenLargerThanMd = useMediaQuery(theme.breakpoints.up('md'));
 
-  useEffect(() => {
-    setElectricity(electricityUsage * 0.85);
-    setGas(gasUsage * 11.7 * 0.02);
-    setCar(carUsage * 0.146);
-  }, [carUsage, electricityUsage, gasUsage, setCar, setElectricity, setGas]);
+    const [expanded, setExpanded] = React.useState('panel1');
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
-  useEffect(() => {
-    setResult(electricity + gas + car);
-  }, [car, electricity, gas, setResult]);
 
-  return (
-    <>
-      {
-        isScreenLargerThanMd &&
-          <img src="/park.svg" alt="environmental drawing" style={{
-            height: 220,
-            position: "absolute",
-            right: 30,
-            bottom: 0,
-          }} />
-      }
-      <Typography variant="h4" sx={{marginTop: '1rem'}}>Your energy consumptions</Typography>
-      <Grid sx={{ marginTop: '1rem'}} item>
-        <Typography variant="h5" sx={{marginTop: '2rem'}}>Electricity Usage</Typography>
-        <TextField
-          type="text"
-          label="Electricity"
-          value={electricityUsage}
-          onChange={(event) => {
-            if (Number.parseInt(event.target.value) >= 0 || event.target.value === '') {
-              setElectricityUsage(event.target.value);
-            }
-          }}
-          variant="outlined"
-          margin="normal"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">Kwh</InputAdornment>,
-          }}
-        />
-      </Grid>
-      <Grid item>
-        <Typography variant="h5" sx={{marginTop: '2rem'}}>Gas Usage</Typography>
-        <TextField
-          type="text"
-          label="Gas"
-          value={gasUsage}
-          onChange={(event) => {
-            if (Number.parseInt(event.target.value) >= 0 || event.target.value === '') {
-              setGasUsage(event.target.value);
-            }
-          }}
-          variant="outlined"
-          margin="normal"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">MJ</InputAdornment>,
-          }}
-        />
-      </Grid>
-      <Grid item>
-        <Typography variant="h5" sx={{marginTop: '2rem'}}>Private Vehicle Usage</Typography>
-        <TextField
-          type="text"
-          label="Private Vehicle"
-          value={carUsage}
-          onChange={(event) => {
-            if (Number.parseInt(event.target.value) >= 0 || event.target.value === '') {
-              setCarUsage(event.target.value);
-            }
-          }}
-          variant="outlined"
-          margin="normal"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">Km</InputAdornment>,
-          }}
-        />
-      </Grid>
-      <Grid item>
-        <Typography variant="h4" sx={{marginTop: '2rem'}}>Your Total Carbonfootprint: {result.toFixed(2)} kg</Typography>
-        <Typography variant="h6">
-          Your Electricity Carbonfootprint: {electricity.toFixed(2)} kg
-        </Typography>
-        <Typography variant="h6">Your Gas Carbonfootprint: {gas.toFixed(2)} kg</Typography>
-        <Typography variant="h6">
-          Your Vehicle Carbonfootprint: {car.toFixed(2)} kg
-        </Typography>
-      </Grid>
-      <Grid item>
-      <Button sx={{marginTop: '2rem', marginRight: '1rem'}} variant="contained" onClick={nextStep}>
-          GET RESULT
-        </Button>
-        <Button sx={{marginTop: '2rem'}} variant="outlined" onClick={lastStep}>
-          Back
-        </Button>
-      </Grid>
-    </>
-  );
+    const handleNumericInputChange = (event, setterFunc) => {
+        const value = event.target.value;
+        const numericRegex = /^[0-9]*$/;  // This regex matches any string that contains only digits.
+        if (numericRegex.test(value) || value === '') {
+            setterFunc(value);
+        }
+    };
+
+    useEffect(() => {
+        setElectricity(electricityUsage * 0.85);
+        setGas(gasUsage * 11.7 * 0.02);
+        setCar(carUsage * 0.146);
+    }, [carUsage, electricityUsage, gasUsage, setCar, setElectricity, setGas]);
+
+    useEffect(() => {
+        setResult(electricity + gas + car);
+    }, [car, electricity, gas, setResult]);
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={12}>
+                <Typography variant="h4" sx={{marginTop: '1rem', textAlign: 'center'}}>Calculate Your Carbon
+                    Footprint</Typography>
+                <Divider></Divider>
+            </Grid>
+
+            <Grid container item xs={12} md={8} alignItems="center" sx={{marginTop: '2rem', alignItems:'start'}}>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <AccordionSummary
+                        // expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Typography sx={{width: '40%', flexShrink: 0}}>
+                            Summary
+                        </Typography>
+                        <Typography sx={{color: 'text.secondary'}}>Summary the cost</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Grid item xs={12} md={12}>
+                            <Grid container className={'inputGroup'} alignItems="center" spacing={2}>
+                                <Grid item xs={12} md={3}>
+                                    <Typography variant="h5">Electricity:</Typography>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField
+                                        type="text"
+                                        label="Electricity"
+                                        value={electricityUsage}
+                                        onChange={(event) => {
+                                            handleNumericInputChange(event, setElectricityUsage);
+                                        }}
+                                        variant="outlined"
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">Kwh</InputAdornment>,
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={5}>
+                                    <Typography variant="h6" sx={{color: '#11C5A7'}}>{electricity.toFixed(2)} kg
+                                        CO2</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <Grid container className={'inputGroup'} alignItems="center" spacing={2}>
+                                <Grid item xs={12} md={3}>
+                                    <Typography variant="h5">Gas:</Typography>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField
+                                        type="text"
+                                        label="Gas"
+                                        value={gasUsage}
+                                        onChange={(event) => {
+                                            handleNumericInputChange(event, setGasUsage);
+                                        }}
+                                        variant="outlined"
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">MJ</InputAdornment>,
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={5}>
+                                    <Typography variant="h6" sx={{color: '#11C5A7'}}>{gas.toFixed(2)} kg
+                                        CO2</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <Grid container className={'inputGroup'} alignItems="center" spacing={2}>
+                                <Grid item xs={12} md={3}>
+                                    <Typography variant="h5">Car:</Typography>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField
+                                        type="text"
+                                        label="Car"
+                                        value={carUsage}
+                                        onChange={(event) => {
+                                            handleNumericInputChange(event, setCarUsage);
+                                        }}
+                                        variant="outlined"
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">km</InputAdornment>,
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={5}>
+                                    <Typography variant="h6" sx={{color: '#11C5A7'}}>{car.toFixed(2)} kg
+                                        CO2</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                    <AccordionSummary
+                        // expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="panel2bh-content"
+                        id="panel2bh-header"
+                    >
+                        <Typography sx={{width: '40%', flexShrink: 0}}>
+                            Summary
+                        </Typography>
+                        <Typography sx={{color: 'text.secondary'}}>Summary the cost</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Grid item xs={12} md={12}>
+                            <Grid container className={'inputGroup'} alignItems="center" spacing={2}>
+                                <Grid item xs={12} md={3}>
+                                    <Typography variant="h5">Electricity:</Typography>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField
+                                        type="text"
+                                        label="Electricity"
+                                        value={electricityUsage}
+                                        onChange={(event) => {
+                                            handleNumericInputChange(event, setElectricityUsage);
+                                        }}
+                                        variant="outlined"
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">Kwh</InputAdornment>,
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={5}>
+                                    <Typography variant="h6" sx={{color: '#11C5A7'}}>{electricity.toFixed(2)} kg
+                                        CO2</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <Grid container className={'inputGroup'} alignItems="center" spacing={2}>
+                                <Grid item xs={12} md={3}>
+                                    <Typography variant="h5">Gas:</Typography>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField
+                                        type="text"
+                                        label="Gas"
+                                        value={gasUsage}
+                                        onChange={(event) => {
+                                            handleNumericInputChange(event, setGasUsage);
+                                        }}
+                                        variant="outlined"
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">MJ</InputAdornment>,
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={5}>
+                                    <Typography variant="h6" sx={{color: '#11C5A7'}}>{gas.toFixed(2)} kg
+                                        CO2</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <Grid container className={'inputGroup'} alignItems="center" spacing={2}>
+                                <Grid item xs={12} md={3}>
+                                    <Typography variant="h5">Car:</Typography>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField
+                                        type="text"
+                                        label="Car"
+                                        value={carUsage}
+                                        onChange={(event) => {
+                                            handleNumericInputChange(event, setCarUsage);
+                                        }}
+                                        variant="outlined"
+                                        InputProps={{
+                                            endAdornment: <InputAdornment position="end">km</InputAdornment>,
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} md={5}>
+                                    <Typography variant="h6" sx={{color: '#11C5A7'}}>{car.toFixed(2)} kg
+                                        CO2</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </AccordionDetails>
+                </Accordion>
+            </Grid>
+
+
+            <Grid item className={'total'} xs={12} md={4} sx={{marginTop: '2rem', alignContent: 'center'}}>
+                <Typography sx={{fontSize: '1rem', color: 'grey'}}>
+                    Total Carbon Footprint
+                </Typography>
+                <Typography sx={{fontSize: '4rem',textShadow: '2px 2px 4px lightgrey'}}>
+                    <span style={{color: '#11C5A7'}}>{result.toFixed(2)}</span>
+                </Typography>
+                <Typography sx={{fontSize: '1rem', color: 'grey'}}>
+                    kg CO2
+                </Typography>
+            </Grid>
+
+            <Grid container item md={12} sx={{marginTop: '1rem'}}>
+                <Grid item xs={12} md={12}>
+                    <Box display="flex" justifyContent="center">
+                        <Button variant="contained"
+                                style={{backgroundColor: 'white', color: theme.palette.primary.main}}
+                                onClick={lastStep}>Back</Button>
+                        <Button variant="contained" color="primary" onClick={nextStep}
+                                sx={{marginLeft: 2}}>Next</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
 }
