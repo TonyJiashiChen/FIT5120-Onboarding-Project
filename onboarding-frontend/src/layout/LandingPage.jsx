15963524b74ce4e4fd40e62c1b42f7263a824343
jirathip-k <jkun0008@student.monash.edu>
2023-08-06 20:25:59 +1000

    fix bug on Safari by using window height in resize event

--- onboarding-frontend/src/layout/LandingPage.jsx
+++ onboarding-frontend/src/layout/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTransition, a } from "react-spring"
 import { useTheme } from "@mui/material";
 import Typography from '@mui/material/Typography';
@@ -9,6 +9,19 @@ import Trail from '../components/Trail'
 
 const LandingPage = (props) => {
     const theme = useTheme()
+
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowHeight(window.innerHeight)
+        }
+        window.addEventListener('resize', handleResize);
+        console.log(windowHeight)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    
 
     const fullscreenPage = useTransition(props.landingToggled,{
         from: {opacity: 1, transform: "scale(1)"},
@@ -23,7 +36,7 @@ const LandingPage = (props) => {
         {fullscreenPage((styles, item) => item && <a.div style={{
             ...styles,
             position: "fixed",
-            maxHeight: "100dvh",
+            height: `${windowHeight}px`,
             padding: '10px',
             top: 0,
             bottom: 0,
@@ -41,7 +54,7 @@ const LandingPage = (props) => {
             <div
                 style={{
                     display: 'flex',
-                    maxHeight: '30vh',
+                    maxHeight: `${windowHeight/3}px`,
                     flexDirection: 'row',
                     marginBottom: '2rem'
                 }}
@@ -75,7 +88,7 @@ const LandingPage = (props) => {
                 
                 <Typography variant="h6" color='common.white'>Calculate Now</Typography>
             </Button>
-            <img src="/environmental_study.svg" style={{ marginTop: 'auto', height: '300px' }} alt="environmental study" />
+            <img src="/environmental_study.svg" style={{ marginTop: 'auto', height: `${windowHeight/3}px` }} alt="environmental study" />
         </a.div>)}
         </div>
     )
