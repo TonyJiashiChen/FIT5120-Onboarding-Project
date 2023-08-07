4a7e0c1ab9510a7ff937a0d3344ee4a65bd2bbb6
TonyJiashiChen <tonychen9661@gmail.com>
2023-08-07 14:47:57 +1000

    request test

--- onboarding-frontend/src/components/Result.jsx
+++ onboarding-frontend/src/components/Result.jsx
@@ -1,11 +1,26 @@
-import { Button, Typography } from '@mui/material';
-import { Chart as ChartJS, BarElement, Title, Tooltip, Legend, CategoryScale, LinearScale } from "chart.js";
-import { useMemo } from 'react';
+import { Button, Typography } from "@mui/material";
+import {
+  Chart as ChartJS,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { useMemo } from "react";
 import { Bar } from "react-chartjs-2";
-import { useTheme } from '@emotion/react';
+import { useTheme } from "@emotion/react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-ChartJS.register(BarElement, Title, Tooltip, Legend, CategoryScale, LinearScale);
+ChartJS.register(
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale
+);
 ChartJS.defaults.font.size = 16;
 const options = {
   responsive: true,
@@ -14,16 +29,12 @@ const options = {
       stacked: true,
     },
     y: {
-      stacked: true
-    }
-  }
+      stacked: true,
+    },
+  },
 };
 
-const labels = [
-  'Electricity',
-  'Gas',
-  'Privite Transport'
-]
+const labels = ["Electricity", "Gas", "Privite Transport"];
 
 export function Result({
   lastStep,
@@ -32,54 +43,95 @@ export function Result({
   car,
   result,
   timeframe,
-  cleanAndRedo
+  cleanAndRedo,
+  suburb,
 }) {
   const theme = useTheme();
-  const isScreenLargerThanMd = useMediaQuery(theme.breakpoints.up('md'));
-  const data = useMemo(() => ({
-    labels,
-    datasets: [
-      {
-        label: 'Carbon footprint',
-        data: [electricity, gas, car],
-        backgroundColor: theme.palette.primary.main
-      },
-      {
-        label: 'Neighbor Average Footprint',
-        data: [0, 0, 0],
-        backgroundColor: theme.palette.secondary.main
-      }
+  const isScreenLargerThanMd = useMediaQuery(theme.breakpoints.up("md"));
+  const data = useMemo(
+    () => ({
+      labels,
+      datasets: [
+        {
+          label: "Carbon footprint",
+          data: [electricity, gas, car],
+          backgroundColor: theme.palette.primary.main,
+        },
+        {
+          label: "Neighbor Average Footprint",
+          data: [0, 0, 0],
+          backgroundColor: theme.palette.secondary.main,
+        },
+      ],
+    }),
+    [
+      car,
+      electricity,
+      gas,
+      theme.palette.primary.main,
+      theme.palette.secondary.main,
     ]
-  }), [car, electricity, gas, theme.palette.primary.main, theme.palette.secondary.main]);
+  );
 
   const convertToYearly = (value) => {
-    return 12 / timeframe.value * value;
-  }
+    return (12 / timeframe.value) * value;
+  };
 
   const getPlaneRounds = () => {
     return (convertToYearly(result) / 0.115 / 758).toFixed(2);
-  }
-
+  };
 
   return (
     <>
-      {
-        isScreenLargerThanMd &&
-          <img src="/nature.svg" alt="environmental drawing" style={{
+      {isScreenLargerThanMd && (
+        <img
+          src="/nature.svg"
+          alt="environmental drawing"
+          style={{
             height: 230,
             position: "absolute",
             right: 20,
             top: 40,
-          }} />
-      }
-      <Typography variant="h4" sx={{marginTop: '1rem'}}>Your yearly carbon emission would be</Typography>
-      <Typography variant="h2" sx={{marginTop: '1rem'}}><b style={{color: theme.palette.primary.main}}>{convertToYearly(result).toFixed(2)} kg </b></Typography>
-      <Typography variant="h5" sx={{marginTop: '2rem'}}>Similar With Carbon Emission</Typography>
-      <Typography variant="h4"><b style={{color: theme.palette.primary.main}}>{getPlaneRounds()}</b> flights from Sydney to Melbourne</Typography>
-      <Typography variant="h5" sx={{marginTop: '3rem'}}>Carbon Footprint By Activity</Typography>
-      <Bar style={{maxWidth: 500, marginTop: '1rem', marginBottom: '1rem'}} options={options} data={data} />
-      <Button style={{marginTop: '2rem', marginRight: '1rem'}} variant="contained" onClick={cleanAndRedo}>Clean and Redo</Button>
-      <Button style={{marginTop: '2rem'}} variant="outlined" onClick={lastStep}>Back</Button>
+          }}
+        />
+      )}
+      <Typography variant="h4" sx={{ marginTop: "1rem" }}>
+        Your yearly carbon emission would be
+      </Typography>
+      <Typography variant="h2" sx={{ marginTop: "1rem" }}>
+        <b style={{ color: theme.palette.primary.main }}>
+          {convertToYearly(result).toFixed(2)} kg{" "}
+        </b>
+      </Typography>
+      <Typography variant="h5" sx={{ marginTop: "2rem" }}>
+        Similar With Carbon Emission
+      </Typography>
+      <Typography variant="h4">
+        <b style={{ color: theme.palette.primary.main }}>{getPlaneRounds()}</b>{" "}
+        flights from Sydney to Melbourne
+      </Typography>
+      <Typography variant="h5" sx={{ marginTop: "3rem" }}>
+        Carbon Footprint By Activity
+      </Typography>
+      <Bar
+        style={{ maxWidth: 500, marginTop: "1rem", marginBottom: "1rem" }}
+        options={options}
+        data={data}
+      />
+      <Button
+        style={{ marginTop: "2rem", marginRight: "1rem" }}
+        variant="contained"
+        onClick={cleanAndRedo}
+      >
+        Clean and Redo
+      </Button>
+      <Button
+        style={{ marginTop: "2rem" }}
+        variant="outlined"
+        onClick={lastStep}
+      >
+        Back
+      </Button>
     </>
-  )
+  );
 }
--- onboarding-frontend/src/components/Welcome.jsx
+++ onboarding-frontend/src/components/Welcome.jsx
@@ -8,27 +8,30 @@ import CardActionArea from "@mui/material/CardActionArea";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { useTheme } from "@mui/material";
-import TodayIcon from '@mui/icons-material/Today';
-import DateRangeIcon from '@mui/icons-material/DateRange';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import TodayIcon from "@mui/icons-material/Today";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-
-const timeframes = [{
-  label: 'One Month',
-  value: 1,
-  icon: <TodayIcon />
-},{
-  label: 'Three Months',
-  value: 3,
-  icon: <DateRangeIcon />
-}, {
-  label: 'One Year',
-  value: 12,
-  icon: <CalendarMonthIcon />
-}]
+const timeframes = [
+  {
+    label: "One Month",
+    value: 1,
+    icon: <TodayIcon />,
+  },
+  {
+    label: "Three Months",
+    value: 3,
+    icon: <DateRangeIcon />,
+  },
+  {
+    label: "One Year",
+    value: 12,
+    icon: <CalendarMonthIcon />,
+  },
+];
 
 export function Welcome({
   nextStep,
@@ -37,94 +40,159 @@ export function Welcome({
   suburb,
   setSuburb,
   postcode,
-  setPostcode
+  setPostcode,
 }) {
   const theme = useTheme();
-  const isScreenLargerThanMd = useMediaQuery(theme.breakpoints.up('md'));
+  const isScreenLargerThanMd = useMediaQuery(theme.breakpoints.up("md"));
 
   const [options, setOptions] = useState([]);
 
   const get_options = async () => {
     const response = await fetch(`${apiUrl}location`);
     return await response.json();
-    
-  }
+  };
 
   useEffect(() => {
-    get_options().then(data => {
+    get_options().then((data) => {
       setOptions(data);
-      console.log(options)
+      console.log(options);
       if (data && data.length > 0) {
         setSuburb(data[0]);
         setPostcode(data[0].postcode);
-    }
+      }
     });
-  }, [])
+  }, []);
 
   return (
     <>
-      {
-        isScreenLargerThanMd &&
-          <img src="/environment_iaus.svg" alt="environmental drawing" style={{
+      {isScreenLargerThanMd && (
+        <img
+          src="/environment_iaus.svg"
+          alt="environmental drawing"
+          style={{
             height: 200,
             position: "absolute",
             right: 0,
             top: 60,
-            transform: 'scaleX(-1)',
-          }} />
-      }
-      <Typography variant="h4" component="div" sx={{ flexGrow: 1, marginTop: '1rem' }}>
+            transform: "scaleX(-1)",
+          }}
+        />
+      )}
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{ flexGrow: 1, marginTop: "1rem" }}
+      >
         Welcome to the Carbon Visualizer!
       </Typography>
       <Typography variant="subtitle1" component="div" sx={{ flexGrow: 1 }}>
-        This tool will help you visualize the carbon footprint of your daily activities.
+        This tool will help you visualize the carbon footprint of your daily
+        activities.
       </Typography>
-      <Typography variant="h5" component="div" sx={{ flexGrow: 1, marginTop: '2rem' }}>
+      <Typography
+        variant="h5"
+        component="div"
+        sx={{ flexGrow: 1, marginTop: "2rem" }}
+      >
         Select a suburb for comparasion
       </Typography>
-      <Autocomplete 
-        disablePortal 
-        options={options} 
+      <Autocomplete
+        disablePortal
+        options={options}
         value={suburb}
         onChange={(event, newValue) => {
           setSuburb(newValue);
           setPostcode(newValue.postcode);
         }}
-        sx={{ maxWidth: 400, marginTop: '1rem' }} 
+        sx={{ maxWidth: 400, marginTop: "1rem" }}
         renderOption={(props, option) => (
           <Box component="li" {...props}>
             {option.suburb} ({option.postcode})
           </Box>
         )}
-        renderInput={(params) => <TextField {...params} label="Enter your suburb" helperText="Compare your carbon consumption with your neighbors." />}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Enter your suburb"
+            helperText="Compare your carbon consumption with your neighbors."
+          />
+        )}
         getOptionLabel={(option) => option.suburb}
         disableClearable
       />
 
-      <Typography variant="h5" component="div" sx={{ flexGrow: 1, marginTop: '2rem' }}>
+      <Typography
+        variant="h5"
+        component="div"
+        sx={{ flexGrow: 1, marginTop: "2rem" }}
+      >
         Select a time period to visualize
       </Typography>
-      <Typography variant="subtitle1" component="div" sx={{ flexGrow: 1, marginTop: '0.5rem' }}>
+      <Typography
+        variant="subtitle1"
+        component="div"
+        sx={{ flexGrow: 1, marginTop: "0.5rem" }}
+      >
         Your selection: <b>{timeframe.label}</b>
       </Typography>
-      
-      <div style={{display: 'flex', flexDirection: 'row', marginTop: '1rem', flexWrap: 'wrap', gap: '1rem'}}>
-      {
-        timeframes.map(tf => (
-          <Card variant={tf.value === timeframe.value?'elevation':'outlined'} sx={{ minWidth: 200, maxWidth: 300, marginTop: '1rem', border: tf.value===timeframe.value?`3px solid ${theme.palette.primary.main}`:`2px dashed ${theme.palette.primary.main}` }}>
-            <CardActionArea onClick={() => {setTimeframe(tf)}}>
-              <CardContent sx={{height: 120, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          marginTop: "1rem",
+          flexWrap: "wrap",
+          gap: "1rem",
+        }}
+      >
+        {timeframes.map((tf) => (
+          <Card
+            variant={tf.value === timeframe.value ? "elevation" : "outlined"}
+            sx={{
+              minWidth: 200,
+              maxWidth: 300,
+              marginTop: "1rem",
+              border:
+                tf.value === timeframe.value
+                  ? `3px solid ${theme.palette.primary.main}`
+                  : `2px dashed ${theme.palette.primary.main}`,
+            }}
+          >
+            <CardActionArea
+              onClick={() => {
+                setTimeframe(tf);
+              }}
+            >
+              <CardContent
+                sx={{
+                  height: 120,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
                 {tf.icon}
-                <Typography gutterBottom variant="h5" component="div" sx={{marginBottom: 0, marginLeft: '5px'}}>
-                   {tf.label}
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  sx={{ marginBottom: 0, marginLeft: "5px" }}
+                >
+                  {tf.label}
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
-        ))
-      }
+        ))}
       </div>
-      <Button sx={{marginTop: '3rem'}} disabled={!(suburb&&timeframe)} variant="contained" onClick={nextStep}>Next</Button>
+      <Button
+        sx={{ marginTop: "3rem" }}
+        disabled={!(suburb && timeframe)}
+        variant="contained"
+        onClick={nextStep}
+      >
+        Next
+      </Button>
     </>
-  )
+  );
 }
