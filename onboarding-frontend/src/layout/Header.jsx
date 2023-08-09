5c2468c5a7a0fa079a3c9661c87b6ca497cc6a8a
cam <cwan0148@student.monash.edu>
2023-08-09 15:42:26 +1000

    comments on Main, link back to welcome page

--- onboarding-frontend/src/layout/Header.jsx
+++ onboarding-frontend/src/layout/Header.jsx
@@ -2,16 +2,19 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <img src='/favicon-white.png' style={{height: '32px', marginRight: '1rem'}} alt='logo' />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Carbon Visualizer
-          </Typography>
+          <Link to="/" style={{ textDecoration: 'none', color: 'white', display: 'flex', flexDirection: 'row' }}>
+            <img src='/favicon-white.png' style={{height: '32px', marginRight: '1rem'}} alt='logo' />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Carbon Visualizer
+            </Typography>
+          </Link>
         </Toolbar>
       </AppBar>
     </>
--- onboarding-frontend/src/layout/Main.jsx
+++ onboarding-frontend/src/layout/Main.jsx
@@ -8,8 +8,8 @@ import Paper from "@mui/material/Paper";
 import { Result } from "../components/Result";
 import { ActivityUsage } from "../components/ActivityUsage";
 
-// steps for carbon footprint calculator
-const steps = [
+// bill based steps for carbon footprint calculator
+const stepsBillMode = [
   {
     title: "Welcome",
     component: <Welcome />,
@@ -24,7 +24,7 @@ const steps = [
   },
 ];
 
-// steps for carbon footprint calculator
+// activity based steps for carbon footprint calculator
 const stepsActivityMode = [
   {
     title: "Welcome",
@@ -41,10 +41,8 @@ const stepsActivityMode = [
 ];
 
 export function Main() {
-
   //step activity mode
   const [stepActivityMode, setStepActivityMode] = useState(true);
-
   // current active step
   const [activeStep, setActiveStep] = useState(0);
   // postcode info
@@ -63,13 +61,18 @@ export function Main() {
   // car carbon emission
   const [car, setCar] = useState(0);
   const [result, setResult] = useState(0);
+  // average electricity usage in the area
   const [averageElectricity, setAverageElectricity] = useState(0);
+  // average gas usage in the area
   const [averageGas, setAverageGas] = useState(0);
+  // average energy usage in the area
   const [averageEnergy, setAverageEnergy] = useState(0);
+  // average total carbon emission in the area
   const [averageResult, setAverageResult] = useState("");
   // activity usages
   const [activityUsages, setActivityUsages] = useState([]);
 
+  // resets all user inputs and results
   const cleanAndRedo = useCallback(() => {
     setActiveStep(0);
     setPostcode("");
@@ -89,88 +92,67 @@ export function Main() {
     setActivityUsages([]);
   }, []);
 
-  const nextStep = () => {
-    setActiveStep(activeStep + 1);
-  };
-  const lastStep = () => {
-    setActiveStep(activeStep - 1);
-  };
+  // go to the next step
+  const nextStep = useCallback(() => {
+    if (activeStep < stepsBillMode.length - 1) {
+      setActiveStep(activeStep + 1);
+    }
+  }, [setActiveStep, activeStep]);
 
+  // go to the previous step
+  const lastStep = useCallback(() => {
+    if (activeStep > 0) {
+      setActiveStep(activeStep - 1);
+    }
+  }, [setActiveStep, activeStep]);
 
   return (
     <>
-      <Container maxWidth="lg" sx={{paddingBottom: '4rem'}}>
+      <Container maxWidth="lg" sx={{ paddingBottom: "4rem" }}>
         <StepIndicator
-          steps={stepActivityMode ? steps:stepsActivityMode}
+          steps={stepActivityMode ? stepsBillMode : stepsActivityMode}
           activeStep={activeStep}
           setActiveStep={setActiveStep}
         />
         <Paper elevation={3} sx={{ padding: "2rem", position: "relative" }}>
-          {stepActivityMode ? React.cloneElement(stepsActivityMode[activeStep].component, {
-            stepActivityMode,
-            setStepActivityMode,
-            activeStep,
-            steps,
-            nextStep,
-            lastStep,
-            suburb,
-            setSuburb,
-            postcode,
-            setPostcode,
-            timeframe,
-            setTimeframe,
-            electricity,
-            setElectricity,
-            gas,
-            setGas,
-            car,
-            setCar,
-            result,
-            setResult,
-            averageElectricity,
-            setAverageElectricity,
-            averageGas,
-            setAverageGas,
-            averageEnergy,
-            setAverageEnergy,
-            averageResult,
-            setAverageResult,
-            cleanAndRedo,
-            activityUsages,
-            setActivityUsages,
-          }):React.cloneElement(steps[activeStep].component, {
-            stepActivityMode,
-            setStepActivityMode,
-            activeStep,
-            steps,
-            nextStep,
-            lastStep,
-            suburb,
-            setSuburb,
-            postcode,
-            setPostcode,
-            timeframe,
-            setTimeframe,
-            electricity,
-            setElectricity,
-            gas,
-            setGas,
-            car,
-            setCar,
-            result,
-            setResult,
-            averageElectricity,
-            setAverageElectricity,
-            averageGas,
-            setAverageGas,
-            averageEnergy,
-            setAverageEnergy,
-            averageResult,
-            setAverageResult,
-            cleanAndRedo,
-            activityUsages,
-            setActivityUsages
-          })}
+          {React.cloneElement(
+            stepActivityMode
+              ? stepsActivityMode[activeStep].component
+              : stepsBillMode[activeStep].component,
+            {
+              stepActivityMode,
+              setStepActivityMode,
+              activeStep,
+              steps: stepsBillMode,
+              nextStep,
+              lastStep,
+              suburb,
+              setSuburb,
+              postcode,
+              setPostcode,
+              timeframe,
+              setTimeframe,
+              electricity,
+              setElectricity,
+              gas,
+              setGas,
+              car,
+              setCar,
+              result,
+              setResult,
+              averageElectricity,
+              setAverageElectricity,
+              averageGas,
+              setAverageGas,
+              averageEnergy,
+              setAverageEnergy,
+              averageResult,
+              setAverageResult,
+              cleanAndRedo,
+              activityUsages,
+              setActivityUsages,
+            }
+          )}
         </Paper>
       </Container>
     </>
